5b78b989a3cb6574239582f9c15f25871426750c
Gonzalo Traverso <gonzalotraverso@gmail.com>
2018-10-27 11:23:16 -0300

    add methods to checkout, to pass test

---
+++ checkout.rb
@@ -0,0 +1,57 @@
+require_relative "data/item_types"
+
+class Checkout
+  attr_reader :items
+
+  def initialize(pricing_rules = [])
+    @items = {}
+    @pricing_rules = pricing_rules
+  end
+
+  def scan(item_code)
+    code = item_code.to_sym
+    item = find_item_by_code(code)
+    add_item_to_basket(item, code)
+  end
+
+  def scan_multiple(*items)
+    items.each { |item| scan(item) }
+  end
+
+  def subtotal
+    @items.inject(0) do |sum, (code, values)| 
+      sum + (values[:price] * values[:count])
+    end.round(2)
+  end
+
+  def total
+    # add_discounts(subtotal)
+    subtotal
+  end
+  
+  def get_item(code)
+    @items[code.to_sym]
+  end
+  
+  def get_item_count(code)
+    get_item(code)[:count]
+  end
+  
+  private
+  def add_discounts(subtotal)
+    #
+  end
+
+  def find_item_by_code(code)
+    item = ITEM_TYPES.select { |item| item[:code] == code }[0]
+    raise ArgumentError, "invalid code" if item.nil?
+    item
+  end
+
+  def add_item_to_basket(item, code)
+    @items[code] = {
+      price: item[:price],
+      count: (@items.dig(code, :count) || 0) + 1
+    }
+  end
+end
